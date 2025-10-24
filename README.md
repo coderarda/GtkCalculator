762949aa3bed1dc4aaa7e737fac6ed49f7008967
Arda Akcagöz <akcagozarda@outlook.com>
2025-10-24 20:36:35 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GtkCalculator
+
+A little experiment of mine trying to do a GUI app in C.
---
+++ build/CMakeFiles/gtk_proj.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtk_proj.dir/main.c.obj"
+  "CMakeFiles/gtk_proj.dir/main.c.obj.d"
+  "gtk_proj.exe"
+  "gtk_proj.exe.manifest"
+  "gtk_proj.pdb"
+  "libgtk_proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gtk_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
